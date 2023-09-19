823d22cac761d0196fbcccf744efce085e936700
akshay.patil <aqshey@gmail.com>
2023-09-19 07:29:41 +0200

    adding first ever commit with dockerfile and commands. and other additional information

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # lambda_mastery
 mastery for lambda function for different use cases
+
+mkdir package
+
+pip install qrcode -t ./package
+
+cp generate_qr_code.py ./package
+
+cd package
+
+zip -r ../function.zip .
+
+cd ..
---
+++ generate_qr_code.py
@@ -0,0 +1,27 @@
+import qrcode
+
+def generate_qr_code(event, context):
+    # Retrieve the parameter from the API Gateway event
+    parameter_value = event['queryStringParameters']['parameter']
+
+    # Generate a QR code with the parameter value
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(parameter_value)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # Return the QR code image as a base64-encoded PNG
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "image/png",
+        },
+        "body": img.save("/tmp/qr_code.png", format="PNG"),
+        "isBase64Encoded": True,
+    }
